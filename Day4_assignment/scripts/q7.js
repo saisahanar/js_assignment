75a5612d026c51f7e144430231f128593ac48aed
sai sahana r <62750345+saisahanar@users.noreply.github.com>
2020-07-20 18:57:31 +0530

    Update q7.js

--- Day4_assignment/scripts/q7.js
+++ Day4_assignment/scripts/q7.js
@@ -1,10 +1,13 @@
-let n=Number(prompt("enter upper bound"));
-for(let i=2;i<=n;i++){
+let input=Number(prompt("enter upper bound:"));
+for(let i=2;i<=input;i++){
+    let flag=true;
     for(let j=2;j<i;j++){
-        if(i%j==0)
-            continue;
+        if(i%j==0){
+            flag=false;
+            break;
+        }
+    }
+    if(flag==true){
         console.log(i);
     }
-       
-    
 }
